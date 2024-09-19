0ad989e78b34c8508dfb2dd2fa9037ef6f5eb362
Chen Gong <88132989+ChenGong57@users.noreply.github.com>
2024-09-19 14:36:53 +0800

    main script for using abCAN

---
+++ predict.py
@@ -0,0 +1,30 @@
+import torch
+import argparse
+
+from modeling import Decoder
+from process_input import *
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('complex_pdb', type=str)
+    parser.add_argument('chains_info', type=str)
+    parser.add_argument('mutation_info', type=str)
+    parser.add_argument('--load_model', default='/home/gongchen/model/ckpts/best/check/model.pt')
+
+    args = parser.parse_args()
+
+    ckpt = torch.load(args.load_model)
+    config = ckpt['config']
+    config_namespace = argparse.Namespace(**config)
+    weight = ckpt['model_state']
+    model = Decoder(config_namespace).cuda()
+    model.load_state_dict(weight)
+    vars(args).update(vars(config_namespace))
+
+    with torch.no_grad():
+        model.eval()
+        protein = Proteins(args.complex_pdb, args.chains_info)
+        mut_proteins = get_chains_dict(args.mutation_info, protein.complex)
+        complex = completize(mut_proteins, args.esm)
+        ddG_pre = model(*complex)
+        print(f'Predicted ddG value for input mutant is: {ddG_pre.item():.3f} kcal/mol.')
